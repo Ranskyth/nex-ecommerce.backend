7e75eed2876be90fffb613d1acc5831e6b87bc75
gabriel <ga.lima2205@gmail.com>
2025-03-24 15:03:57 -0300

    up

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,6 @@ import cors from "@fastify/cors"
 
 import { productsRoutes } from "./routes/productsRoutes";
 import { mercadopagoRoutes } from "./routes/mercadopagoRoutes";
-import { authRouter } from "./routes/authRoutes";
-
-
 
 const PORT = Number(process.env.PORT)
 
@@ -13,6 +10,5 @@ const app = fastify();
 app.register(cors)
 app.register(productsRoutes)
 app.register(mercadopagoRoutes)
-
 
 app.listen({port: PORT},() => {console.log(`server on in port http://0.0.0.0:${PORT}`)});
