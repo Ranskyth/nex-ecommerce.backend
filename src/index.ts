74bd62a66de305be55bd3b8c1221e45299b32a4d
gabriel <ga.lima2205@gmail.com>
2025-03-25 17:59:21 -0300

    feat: add cors config

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,14 @@ import cors from "@fastify/cors"
 
 import { productsRoutes } from "./routes/productsRoutes";
 import { mercadopagoRoutes } from "./routes/mercadopagoRoutes";
+import { userRoutes } from "./routes/userRoutes";
 
 const app = fastify();
-app.register(cors)
+app.register(cors, {
+    origin: "*",
+    methods: ["GET","POST","PUT","DELETE"],
+})
+app.register(userRoutes)
 app.register(productsRoutes)
 app.register(mercadopagoRoutes)
 
